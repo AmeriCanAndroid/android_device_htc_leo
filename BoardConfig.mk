2dd1b0e62ad6a56f22e69452976367347ba7030a
AmeriCanAndroid <americanandroidrom@gmail.com>
2011-04-25 11:27:24 -0700

    Edited BoardConfig.mk via GitHub

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -105,4 +105,4 @@ BOARD_FLASH_BLOCK_SIZE := 131072
 
 
 TARGET_RELEASETOOLS_EXTENSIONS := device/htc/common
-TARGET_PREBUILT_KERNEL := device/htc/leo/kernel
+TARGET_PREBUILT_KERNEL := device/htc/leo/zImage
