872f37c3f60a936fc0b095f70586dbe98b4f4a80
AmeriCanAndroid <americanandroidrom@gmail.com>
2011-04-25 20:07:47 -0700

    Edited full_leo.mk via GitHub

--- full_leo.mk
+++ full_leo.mk
@@ -21,6 +21,7 @@
 
 # Inherit from those products. Most specific first.
 $(call inherit-product, $(SRC_TARGET_DIR)/product/languages_full.mk)
+$(call inherit-product, device/htc/leo/leo_us.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full.mk)
 
 # Discard inherited values and use our own instead.
