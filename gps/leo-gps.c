b44cb815e001a6ea83813f315672b9c78d24e4be
Warren <warren@VBOX-ACA.(none)>
2011-06-05 15:33:47 -0700

    Modded the GPS further with code found on the interwebs.

--- gps/leo-gps.c
+++ gps/leo-gps.c
@@ -41,6 +41,102 @@ extern int init_gps_rpc();
 
 /* this is the state of our connection */
 
+
+/* XTRA */
+typedef struct {
+    gps_xtra_download_request download_request_cb;
+} OldGpsXtraCallbacks;
+
+typedef struct {
+    int  (*init)( OldGpsXtraCallbacks* callbacks );
+    int  (*inject_xtra_data)( char* data, int length );
+} OldGpsXtraInterface;
+
+/* AGPS */
+typedef struct {
+    AGpsType        type;
+    AGpsStatusValue status;
+} OldAGpsStatus;
+
+typedef void (* old_agps_status_callback)(OldAGpsStatus* status);
+
+typedef struct {
+    old_agps_status_callback status_cb;
+} OldAGpsCallbacks;
+
+typedef struct {
+    void  (*init)( OldAGpsCallbacks* callbacks );
+    int  (*data_conn_open)( const char* apn );
+    int  (*data_conn_closed)();
+    int  (*data_conn_failed)();
+    int  (*set_server)( AGpsType type, const char* hostname, int port );
+} OldAGpsInterface;
+
+/* NI */
+typedef struct
+{
+    void (*init) (GpsNiCallbacks *callbacks);
+    void (*respond) (int notif_id, GpsUserResponseType user_response);
+} OldGpsNiInterface;
+
+/* Core structures */
+typedef struct {
+    uint16_t        flags;
+    double          latitude;
+    double          longitude;
+    double          altitude;
+    float           speed;
+    float           bearing;
+    float           accuracy;
+    GpsUtcTime      timestamp;
+} OldGpsLocation;
+
+typedef struct {
+    GpsStatusValue status;
+} OldGpsStatus;
+
+typedef struct {
+    int     prn;
+    float   snr;
+    float   elevation;
+    float   azimuth;
+} OldGpsSvInfo;
+
+typedef struct {
+    int         num_svs;
+    OldGpsSvInfo   sv_list[GPS_MAX_SVS];
+    uint32_t    ephemeris_mask;
+    uint32_t    almanac_mask;
+    uint32_t    used_in_fix_mask;
+} OldGpsSvStatus;
+
+
+/* Core GPS HAL Callbacks */
+typedef void (* old_gps_location_callback)(OldGpsLocation* location);
+typedef void (* old_gps_status_callback)(OldGpsStatus* status);
+typedef void (* old_gps_sv_status_callback)(OldGpsSvStatus* sv_info);
+
+typedef struct {
+    old_gps_location_callback location_cb;
+    old_gps_status_callback status_cb;
+    old_gps_sv_status_callback sv_status_cb;
+    gps_nmea_callback nmea_cb;
+} OldGpsCallbacks;
+
+
+typedef struct {
+    int   (*init)( OldGpsCallbacks* callbacks );
+    int   (*start)( void );
+    int   (*stop)( void );
+    void  (*cleanup)( void );
+    int   (*inject_time)(GpsUtcTime time, int64_t timeReference,
+            int uncertainty);
+    int  (*inject_location)(double latitude, double longitude, float accuracy);
+    void  (*delete_aiding_data)(GpsAidingData flags);
+    int   (*set_position_mode)(GpsPositionMode mode, int fix_frequency);
+    const void* (*get_extension)(const char* name);
+} OldGpsInterface;
+
 typedef struct {
     const char*  p;
     const char*  end;
@@ -177,8 +273,8 @@ typedef struct {
 typedef struct {
 	int                     init;
 	int                     fd;
-	GpsCallbacks            callbacks;
-	GpsStatus status;
+	OldGpsCallbacks            callbacks;
+	OldGpsStatus status;
 	pthread_t               thread;
 	int                     control[2];
 } GpsState;
@@ -790,7 +886,7 @@ Fail:
 /*****************************************************************/
 
 
-static int gps_init(GpsCallbacks* callbacks) {
+static int gps_init(OldGpsCallbacks* callbacks) {
 	GpsState*  s = _gps_state;
 	
 	memset(&s->status, 0, sizeof(&s->status));
@@ -873,7 +969,7 @@ static const void* gps_get_extension(const char* name) {
 	return NULL;
 }
 
-static const GpsInterface  hardwareGpsInterface = {
+static const OldGpsInterface  sLocEngInterface = {
     sizeof(GpsInterface),
     gps_init,
     gps_start,
@@ -886,8 +982,16 @@ static const GpsInterface  hardwareGpsInterface = {
     gps_get_extension,
 };
 
-const GpsInterface* gps_get_hardware_interface()
+const OldGpsInterface* gps_get_hardware_interface()
 {
-    return &hardwareGpsInterface;
+    return &sLocEngInterface;
+}
+
+EXTERN_SYMBOL(get_gps_hardware_interface);
+
+// for gps.c
+const OldGpsInterface* get_gps_interface()
+{
+	return &sLocEngInterface;
 }
 
