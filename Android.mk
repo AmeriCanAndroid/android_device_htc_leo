44109045ba9731b143360e17d28501c9c1c4bf8f
AmeriCanAndroid <americanandroidrom@gmail.com>
2011-08-15 21:33:24 -0600

    leo: Add build barrier for other devices (just incase) https://github.com/CyanogenMod/android_device_htc_leo/blob/b8e67364ce8dc7280cfc1acca7e19b10e52d6eb7/Android.mk --- td&h

--- Android.mk
+++ Android.mk
@@ -12,6 +12,8 @@
 # See the License for the specific language governing permissions and
 # limitations under the License.
 
+ifeq ($(TARGET_BOOTLOADER_BOARD_NAME),leo)
+
 LOCAL_PATH := $(my-dir)
 subdir_makefiles := \
 	$(LOCAL_PATH)/libreference-ril/Android.mk \
@@ -22,3 +24,5 @@ subdir_makefiles := \
         $(LOCAL_PATH)/core/Android.mk
 
 include $(subdir_makefiles)
+
+endif
