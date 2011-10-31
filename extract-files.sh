d3351ef890c8382e09323af3956b5e5cc5d9d332
AmeriCanAndroid <americanandroidrom@gmail.com>
2011-10-30 20:27:45 -0600

    make changes to arif's extract files until it gets sorted out. (without this temp revision, prop files are not stored in the correct location ~/android/system/vendor/htc/leo/proprietary/ *(HERE) -- They are stored in subfolders *(HERE) being etc bin lib. and the compile process doesnt know to look past the proprietary/ folder for the files)

--- extract-files.sh
+++ extract-files.sh
@@ -24,10 +24,10 @@ rm -rf $BASE/*
 
 for FILE in `cat proprietary-files.txt | grep -v ^# | grep -v ^$`; do
     DIR=`dirname $FILE`
-    if [ ! -d $BASE/$DIR ]; then
-        mkdir -p $BASE/$DIR
-    fi
-    adb pull /system/$FILE $BASE/$FILE
+    #if [ ! -d $BASE/$DIR ]; then
+    #    mkdir -p $BASE/$DIR
+    #fi
+    adb pull /system/$FILE $BASE/ #$FILE
 done
 
 ./setup-makefiles.sh
